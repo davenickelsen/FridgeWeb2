3bb3fab004c565fe0d73c54e23a380a255568279
drn417 <drn417@sherwin.com>
2018-08-26 21:58:11 -0400

    Added untested user create API

---
+++ FridgeWeb2/Api/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FridgeData.Authorization;
+using FridgeWeb2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FridgeWeb2.Api
+{
+    [Route("api/Users")]
+    public class UsersController
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public bool Post([FromBody] CreateUserModel model)
+        {
+            if (model.Token != Environment.GetEnvironmentVariable("FRIDGE_API_TOKEN"))
+            {
+                throw new UnauthorizedAccessException("Bad key");
+            }
+
+            var user = _userService.FindByIdAsync(model.UserName).GetAwaiter().GetResult();
+            if (user != null)
+            {
+                throw new Exception("User already exists");
+            }
+
+            return _userService.CreateAsync(new AppUser {Id = model.UserName}, model.Password).GetAwaiter().GetResult() == IdentityResult.Success;
+        }
+        
+        
+    }
+}
---
+++ FridgeWeb2/Models/CreateUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FridgeWeb2.Models
+{
+    public class CreateUserModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
